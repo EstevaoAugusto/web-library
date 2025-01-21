cc9bcbb2e09b12987eacf357d7ba56170311591c
Estevão Augusto <estevaoaugusto2005@gmail.com>
2025-01-21 17:58:11 -0300

    feat: adicionado componente Header

---
+++ src/frontend/src/components/Header.jsx
@@ -0,0 +1,21 @@
+import PersonIcon from "@mui/icons-material/Person" 
+import IconButton from "@mui/material/IconButton"
+import styles from "../styles/Header.module.css"
+import { useNavigate } from "react-router"
+
+
+const Header = () => {
+    const navegar = useNavigate()
+    
+    return (
+        <header className={styles.header}>
+            <input type="text" id="fpesquisa" name="fpesquisa" placeholder='Pesquisa'/>
+
+            <IconButton onClick={() => navegar("/TelaPerfilLeitor")}>
+                <PersonIcon/>
+            </IconButton>
+        </header>
+    )
+}
+
+export default Header
